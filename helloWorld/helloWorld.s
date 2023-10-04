239b83dd773cb49c7f50e03d52ef2e0d20a9e9bc
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-10-04 18:59:44 -0500

    Added comments to helloWorld

--- helloWorld/helloWorld.s
+++ helloWorld/helloWorld.s
@@ -1,15 +1,15 @@
 .global _start
-.align 2
+.align 2                ; align to a multiple of 2 bytes
 
 _start:
-    mov X0, #1
-    adr X1, helloworld
-    mov X2, #13
-    mov X16, #4
-    svc #0x80
+    mov X0, #1          ; move stdout (1) to file descriptor (X0)
+    adr X1, helloworld  ; load &helloworld into X1
+    mov X2, #13         ; move length of helloworld (13) to X2 (GP reg)
+    mov X16, #4         ; move write (4) to syscall reg (X16)
+    svc #0x80           ; system call
 
-    mov X0, #0
-    mov X16, #1
-    svc #0x80
+    mov X0, #0          ; move exit status (0) to exit status (X0)
+    mov X16, #1         ; move exit (1) to syscall number (X16)
+    svc #0x80           ; system call
 
 helloworld: .ascii "Hello World!\n"
