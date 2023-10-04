a2acdac24c921f36c05bba6fcd23eddfac12a717
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-10-04 18:03:29 -0500

    Fixed comments

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ extern "C" void timeTest(int iterations);
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << "[number of iterations]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [number of iterations]" << std::endl;
     }
     
     int x = std::atoi(argv[1]);
     std::cout << x << std::endl;
-    timeTest(x);
+    timeTest(x); // Call assembly
     return 0;
 }
